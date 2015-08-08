48aa0b1a0f0348fb1ffce5d54bcdcc7c695dae18
Tomlin Leniston <leniston@gmail.com>
2015-08-08 19:38:50 +1000

    refactored from procedural to object oriented

--- README.md
+++ README.md
@@ -1,28 +1,26 @@
 # Djaws
 ### A small Django app for publishing the contents of a directory to an Amazon S3 bucket and configuring the bucket to serve the contents as a static website  
 
-##### To do
-* Package it up
+#### To do
+* Add Route 53 / domain configuration
 * Add tests
-* Add Route 53 / Domain configuration
+* Package it up
 
 #### Usage
-* Add configuration to `settings.py`
-```
-# Example Djaws configuration
+**Add configuration to `settings.py`**
 
-CONTENT_DIRECTORY = os.path.join(BASE_DIR, 'your_static_site_directory_name')
-AWS_ACCESS_KEY_ID = 'your access key ID'
-AWS_SECRET_ACCESS_KEY = 'your secret access key'
-```
+    # Example Djaws configuration
 
-* Publish your site
-`bucket_name` must be a string that complies with [s3 bucket naming rules](http://docs.aws.amazon.com/AmazonS3/latest/dev/BucketRestrictions.html#bucketnamingrules) 
-```
-publish_site('bucket_name')
-```
+    CONTENT_DIRECTORY = os.path.join(BASE_DIR, 'static_site_directory')
+    AWS_ACCESS_KEY_ID = 'your access key ID'
+    AWS_SECRET_ACCESS_KEY = 'your secret access key'
 
-* Delete your site
-```
-delete_site('bucket_name')
-```
+**Publish your site**
+
+ `bucket_name` must be a string that complies with the [s3 bucket naming rules](http://docs.aws.amazon.com/AmazonS3/latest/dev/BucketRestrictions.html#bucketnamingrules) 
+
+    publish_site('bucket_name')
+
+**Delete your site**
+
+    delete_site('bucket_name')
--- utils.py
+++ utils.py
@@ -1,86 +1,107 @@
 import os
+import re
 from django.conf import settings
 from boto import connect_s3
 from boto.s3.key import Key
 
 
-def get_or_create_bucket(bucket_name, connection):
-    """
-    Gets an s3 bucket by name or creates a new bucket if the bucket does not
-    exist, then configures the bucket as a website
-    """
-    bucket = connection.create_bucket(bucket_name, policy='public-read')
-    bucket.configure_website(suffix='index.html', error_key='error.html')
-    return bucket
+class Site:
+    def get_or_create_bucket(self, connection=None):
+        """
+        Gets an s3 bucket by name or creates a new bucket if the bucket does not
+        exist, then configures the bucket as a website
+        """
+        if connection is None:
+            connection = connect_s3(self.aws_access_key_id,
+                                    self.aws_secret_access_key)
+        bucket = connection.create_bucket(self.name, policy='public-read')
+        bucket.configure_website(suffix='index.html', error_key='error.html')
 
+    def create_file_dict(self):
+        """
+        Creates a dictionary of files from the content_directory with relative
+        paths as keys and absolute paths as values (keys for files at root level
+        do not include the './' prefix)
+        """
+        site_files = {}
+        for root, dir_names, file_names in os.walk(self.content_directory):
+            for file_name in file_names:
+                rel_path = os.path.relpath(root, self.content_directory)
+                if rel_path == '.':
+                    key = file_name
+                else:
+                    key = os.path.join(rel_path, file_name)
+                value = os.path.join(root, file_name)
+                site_files[key] = value
+        self.site_files = site_files
 
-def create_file_dict(content_directory):
-    """
-    Creates a dictionary of files from the content_directory with relative
-    paths as keys and absolute paths as values (keys for files at root level do
-    not include the './' prefix)
-    """
-    site_files = {}
-    for root, dir_names, file_names in os.walk(content_directory):
-        for file_name in file_names:
-            rel_path = os.path.relpath(root, content_directory)
-            if rel_path == '.':
-                key = file_name
-            else:
-                key = os.path.join(rel_path, file_name)
-            value = os.path.join(root, file_name)
-            site_files[key] = value
-    return site_files
+    def upload_site_files(self, bucket=None, connection=None):
+        """
+        Uploads files in site_files dictionary to bucket
+        """
+        if connection is None:
+            connection = connect_s3(self.aws_access_key_id,
+                                    self.aws_secret_access_key)
+        if bucket is None:
+            bucket = connection.get_bucket(self.name)
+        for key, value in self.site_files.iteritems():
+            k = Key(bucket)
+            k.key = key
+            k.set_contents_from_filename(value, policy='public-read')
 
+    def delete_redundant_objects(self, bucket=None, connection=None):
+        """
+        Deletes objects from bucket that aren't in site_files dictionary
+        """
+        if connection is None:
+            connection = connect_s3(self.aws_access_key_id,
+                                    self.aws_secret_access_key)
+        if bucket is None:
+            bucket = connection.get_bucket(self.name)
+        bucket_list_result = bucket.list()
+        obsolete_keys = []
+        for key in bucket_list_result:
+            if key.name not in self.site_files:
+                obsolete_keys.append(key.name)
+        bucket.delete_keys(obsolete_keys)
 
-def upload_site_files(bucket, site_files):
-    """
-    Uploads files in site_files dictionary to bucket
-    """
-    for key, value in site_files.iteritems():
-        k = Key(bucket)
-        k.key = key
-        k.set_contents_from_filename(value, policy='public-read')
+    def publish_site(self):
+        """
+        Gets existing bucket or creates and configures a new bucket as a website,
+        uploads files from the content_directory, then deletes redundant objects
+        from the bucket
+        """
+        connection = connect_s3(self.aws_access_key_id,
+                                self.aws_secret_access_key)
+        self.get_or_create_bucket(connection=connection)
+        bucket = connection.get_bucket(self.name)
+        self.create_file_dict()
+        self.upload_site_files(bucket=bucket, connection=connection)
+        self.delete_redundant_objects(bucket=bucket, connection=connection)
 
+    def delete_site(self):
+        """
+        Deletes the s3 bucket
+        """
+        connection = connect_s3(self.aws_access_key_id,
+                                self.aws_secret_access_key)
+        bucket = connection.get_bucket(self.name)
+        bucket_list_result = bucket.list()
+        bucket.delete_keys([key.name for key in bucket_list_result])
+        bucket.delete()
 
-def delete_redundant_s3_objects(bucket, site_files):
-    """
-    Deletes objects from bucket that aren't in site_files dictionary
-    """
-    bucket_list_result = bucket.list()
-    obsolete_keys = []
-    for key in bucket_list_result:
-        if key.name not in site_files:
-            obsolete_keys.append(key.name)
-    bucket.delete_keys(obsolete_keys)
+    def validate_site_name(self, name):
+        allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
+        if all(allowed.match(x) for x in name.split(".")):
+            self.name = name
+        else:
+            raise ValueError('{} is not a DNS compliant name.'.format(name))
 
-
-def publish_site(bucket_name,
+    def __init__(self, name,
                  aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
                  aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
                  content_directory=settings.CONTENT_DIRECTORY):
-    """
-    Gets existing bucket or creates and configures a new bucket as a website,
-    uploads files from the content_directory, then deletes redundant objects
-    from the bucket
-    """
-    connection = connect_s3(aws_access_key_id=aws_access_key_id,
-                            aws_secret_access_key=aws_secret_access_key)
-    bucket = get_or_create_bucket(bucket_name, connection)
-    site_files = create_file_dict(content_directory)
-    upload_site_files(bucket, site_files)
-    delete_redundant_s3_objects(bucket, site_files)
-
-
-def delete_site(bucket_name,
-                aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-                aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY):
-    """
-    Deletes the s3 bucket
-    """
-    connection = connect_s3(aws_access_key_id=aws_access_key_id,
-                            aws_secret_access_key=aws_secret_access_key)
-    bucket = connection.get_bucket(bucket_name)
-    bucket_list_result = bucket.list()
-    bucket.delete_keys([key.name for key in bucket_list_result])
-    bucket.delete()
+        self.validate_site_name(name)
+        self.aws_access_key_id = aws_access_key_id
+        self.aws_secret_access_key = aws_secret_access_key
+        self.content_directory = content_directory
